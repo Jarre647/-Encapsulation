3b04dd10d5aad6719654b987c5938e230db8e44a
Aleksandr <KirpichnikovAG@suek.ru>
2021-04-03 12:49:28 +0700

    Initial commit

---
+++ ConsoleApp2/Point.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Point
+    {
+        private double x;
+        private double y;
+
+        public static Point CreatePoint(double x, double y)
+        {
+            var point = new Point();
+            point.x = x;
+            point.y = y;
+            return point;
+        }
+
+        public double getDistance(Point other)
+        {
+            double dx = this.x - other.x;
+            double dy = this.y - other.y;
+            return Math.Sqrt(dx * dx + dy * dy);
+        }
+
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p1 = Point.CreatePoint(3, 4);
+            var p2 = Point.CreatePoint(1, 2);
+
+            var distance = p1.getDistance(p2);
+        }
+    }
+}
